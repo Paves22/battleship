5470230b7279e0d51c4e844fa27217fb11ae3f20
adamsurge <102584836@student.swin.edu.au>
2019-09-06 22:47:44 +1000

    Error fixes

--- battleships/src/GameLogic.cs
+++ battleships/src/GameLogic.cs
@@ -1,27 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using SwinGameSDK;
 
-static class GameLogic
+namespace BattleShip
 {
-    public static void Main()
+    static class GameLogic
     {
-        // Opens a new Graphics Window
-        SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
-
-        // Load Resources
-        LoadResources();
-
-        SwinGame.PlayMusic(GameMusic("Background"));
-
-        // Game Loop
-        do
+        public static void Main()
         {
-            HandleUserInput();
-            DrawScreen();
+            // Opens a new Graphics Window
+            SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
+
+
+            // Load Resources
+            GameResources.LoadResources();
+
+            SwinGame.PlayMusic(GameResources.GameMusic("Background"));
+
+            // Game Loop
+            do
+            {
+                GameController.HandleUserInput();
+                GameController.DrawScreen();
+            }
+            while (!SwinGame.WindowCloseRequested() == true | GameController.CurrentState == GameState.Quitting);
+
+            SwinGame.StopMusic();
+
+            // Free Resources and Close Audio, to end the program.
+            GameResources.FreeResources();
         }
-        while (!SwinGame.WindowCloseRequested() == true | CurrentState == GameState.Quitting);
 
-        SwinGame.StopMusic();
-
-        // Free Resources and Close Audio, to end the program.
-        FreeResources();
     }
 }
--- battleships/src/Model/Direction.cs
+++ battleships/src/Model/Direction.cs
@@ -1,14 +1,15 @@
-''' <summary>
-''' The direction the ship can be oriented.
-''' </summary>
-Public Enum Direction
-    ''' <summary>
-    ''' The ship is oriented left/right
-    ''' </summary>
-    LeftRight
+namespace BattleShip
+{
+    public enum Direction
+    {
+        /// <summary>
+        ///     ''' The ship is oriented left/right
+        ///     ''' </summary>
+        LeftRight,
 
-    ''' <summary>
-    ''' The ship is oriented up/down
-    ''' </summary>
-    UpDown
-End Enum
+        /// <summary>
+        ///     ''' The ship is oriented up/down
+        ///     ''' </summary>
+        UpDown
+    }
+}
