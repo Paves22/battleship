c0d2ba9ab8ab93b05101da0e3bedf0f54177fa0e
adamsurge <102584836@student.swin.edu.au>
2019-09-26 12:10:14 +1000

    mouse placement bug fix & build from visual studio

--- battleships/src/Model/Direction.cs
+++ battleships/src/Model/Direction.cs
@@ -5,20 +5,23 @@ using System.Collections;
 using System.Collections.Generic;
 //using System.Data;
 using System.Diagnostics;
-/// <summary>
-/// The direction the ship can be oriented.
-/// </summary>
-public enum Direction
+namespace battleships
 {
-	/// <summary>
-	/// The ship is oriented left/right
-	/// </summary>
-	LeftRight,
+    /// <summary>
+    /// The direction the ship can be oriented.
+    /// </summary>
+    public enum Direction
+    {
+        /// <summary>
+        /// The ship is oriented left/right
+        /// </summary>
+        LeftRight,
 
-	/// <summary>
-	/// The ship is oriented up/down
-	/// </summary>
-	UpDown
+        /// <summary>
+        /// The ship is oriented up/down
+        /// </summary>
+        UpDown
+    }
 }
 
 //=======================================================
